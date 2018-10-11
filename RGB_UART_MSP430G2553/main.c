eb84fb3f85e6fe85c64789e684ce51b63e22ea4a
Nate Hoffman <nhoffman15@me.com>
2018-10-11 16:25:57 -0400

    Remove the example code.

--- RGB_UART_MSP430G2553/main.c
+++ RGB_UART_MSP430G2553/main.c
@@ -1,4 +1,3 @@
-/*
 #include <msp430.h> 
 
 void configureUARTLED () {
@@ -9,8 +8,8 @@ void configureUARTLED () {
 
 void configureLEDPWM () {
     // Red = P1.6 TA0 CCR0
-    // Green = 2.1 TA1 CCR1
-    // Blue = 2.4 TA1 CCR2
+    // Green = P2.1 TA1 CCR1
+    // Blue = P2.4 TA1 CCR2
 
     // P1.6 Config
     P1SEL |= BIT6;              //
@@ -64,9 +63,6 @@ void configureUART () {
 
     UCA0CTL1 &= ~UCSWRST;       // Disables UART reset
     UC0IE |= UCA0RXIE;          // Enable UART based interrupt
-    __bis_SR_register(LPM0_bits + GIE);
-                                // Enter low power mode 0 and enable
-                                // global interrupts
 }
 
 int main(void) {
@@ -74,75 +70,22 @@ int main(void) {
     configureLEDPWM();
     configureTimer();
     configureUART();
-    return 0;
+    __bis_SR_register(LPM0_bits + GIE);
+                                // Enter low power mode 0 and enable
+                                // global interrupts
 }
-
 
 #pragma vector=USCIAB0RX_VECTOR
 __interrupt void USCI0RX_ISR(void) {
     P1OUT |= BIT0;              // Turns on onboard LED when UART
                                 // message is received
-    //char data = UCA0RXBUF;      // Stores the data that is received
+    char data = UCA0RXBUF;      // Stores the data that is received
                                 // in the RX buffer and clears the flags
 
     // TESTING ONLY
-    //TA0CCR1 = data;
+    TA0CCR1 = data;
+    TA1CCR1 = data;
+    TA1CCR2 = data;
 
-    //P1OUT &= ~BIT0;             // Turns off onboard LED
-}
-*/
-
-#include "msp430.h"
-
-#define TXLED BIT0
-#define RXLED BIT0
-#define TXD BIT2
-#define RXD BIT1
-
-const char string[] = { "Hello World\r\n" };
-unsigned int i; //Counter
-
-int main(void)
-{
-   WDTCTL = WDTPW + WDTHOLD; // Stop WDT
-   DCOCTL = 0; // Select lowest DCOx and MODx settings
-   BCSCTL1 = CALBC1_1MHZ; // Set DCO
-   DCOCTL = CALDCO_1MHZ;
-   P2DIR |= 0xFF; // All P2.x outputs
-   P2OUT &= 0x00; // All P2.x reset
-   P1SEL |= RXD + TXD ; // P1.1 = RXD, P1.2=TXD
-   P1SEL2 |= RXD + TXD ; // P1.1 = RXD, P1.2=TXD
-   P1DIR |= RXLED + TXLED;
-   P1OUT &= 0x00;
-   UCA0CTL1 |= UCSSEL_2; // SMCLK
-   UCA0BR0 = 0x08; // 1MHz 115200
-   UCA0BR1 = 0x00; // 1MHz 115200
-   UCA0MCTL = UCBRS2 + UCBRS0; // Modulation UCBRSx = 5
-   UCA0CTL1 &= ~UCSWRST; // **Initialize USCI state machine**
-   UC0IE |= UCA0RXIE; // Enable USCI_A0 RX interrupt
-   __bis_SR_register(CPUOFF + GIE); // Enter LPM0 w/ int until Byte RXed
-   while (1)
-   { }
-}
-
-#pragma vector=USCIAB0TX_VECTOR
-__interrupt void USCI0TX_ISR(void)
-{
-   P1OUT |= TXLED;
-     UCA0TXBUF = string[i++]; // TX next character
-    if (i == sizeof string - 1) // TX over?
-       UC0IE &= ~UCA0TXIE; // Disable USCI_A0 TX interrupt
-    P1OUT &= ~TXLED; }
-
-#pragma vector=USCIAB0RX_VECTOR
-__interrupt void USCI0RX_ISR(void)
-{
-   P1OUT |= RXLED;
-    if (UCA0RXBUF == 'a') // 'a' received?
-    {
-       i = 0;
-       UC0IE |= UCA0TXIE; // Enable USCI_A0 TX interrupt
-      UCA0TXBUF = string[i++];
-    }
-    P1OUT &= ~RXLED;
+    P1OUT &= ~BIT0;             // Turns off onboard LED
 }
